8767e06899cf9fcca31147e611226a832c3df304
Alejandro Hernández <info@alejandrohdezma.com>
2020-05-08 07:10:55 +0200

    Update organize-imports to 0.3.0

--- sbt-scalafix-defaults/src/main/scala/com/alejandrohdezma/sbt/scalafix/defaults/ScalafixDependenciesPlugin.scala
+++ sbt-scalafix-defaults/src/main/scala/com/alejandrohdezma/sbt/scalafix/defaults/ScalafixDependenciesPlugin.scala
@@ -29,7 +29,7 @@ object ScalafixDependenciesPlugin extends AutoPlugin {
     /** Scalafix dependencies added by this plugin */
     lazy val scalafixDefaultDependencies: Seq[ModuleID] = Seq(
       "com.eed3si9n.fix"     %% "scalafix-noinfer" % "0.1.0-M1",
-      "com.github.liancheng" %% "organize-imports" % "0.2.1",
+      "com.github.liancheng" %% "organize-imports" % "0.3.0",
       "com.github.vovapolu"  %% "scaluzzi"         % "0.1.7"
     )
 
