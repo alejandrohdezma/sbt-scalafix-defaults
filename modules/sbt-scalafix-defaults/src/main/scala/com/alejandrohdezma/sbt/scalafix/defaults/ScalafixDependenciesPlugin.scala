4424639f2f0cecce57dfe2efbc4702aa83954a17
Alejandro Hernández <info@alejandrohdezma.com>
2023-07-10 08:46:35 +0200

    Remove `OrganizeImports` since it is included by default
    Signed-off-by: Alejandro Hernández <info@alejandrohdezma.com>

--- modules/sbt-scalafix-defaults/src/main/scala/com/alejandrohdezma/sbt/scalafix/defaults/ScalafixDependenciesPlugin.scala
+++ modules/sbt-scalafix-defaults/src/main/scala/com/alejandrohdezma/sbt/scalafix/defaults/ScalafixDependenciesPlugin.scala
@@ -18,7 +18,7 @@ package com.alejandrohdezma.sbt.scalafix.defaults
 
 import sbt._
 
-/** This plugin just exposes the default Scalafix dependencies to they can be automatically updated by `scala-steward`.
+/** This plugin just exposes the default Scalafix dependencies so they can be automatically updated by `scala-steward`.
   */
 object ScalafixDependenciesPlugin extends AutoPlugin {
 
@@ -26,8 +26,7 @@ object ScalafixDependenciesPlugin extends AutoPlugin {
 
     /** Scalafix dependencies added by this plugin */
     lazy val scalafixDefaultDependencies: Seq[ModuleID] = Seq(
-      "com.github.liancheng" %% "organize-imports" % "0.6.0",
-      "com.github.vovapolu"  %% "scaluzzi"         % "0.1.23"
+      "com.github.vovapolu"  %% "scaluzzi" % "0.1.23"
     )
 
   }
